f5ccd1fcdd8046da5ab694c49a799a7172f6e4bd
wazenmai <iotbwu0101@gmail.com>
2024-06-15 20:10:59 +0800

    update qwen script

--- scripts/qwen/freq-merge.sh
+++ scripts/qwen/freq-merge.sh
@@ -13,12 +13,13 @@ accelerate launch --config_file static/finetune_config.yaml \
   --dominant="frequency" \
   --similarity_base="router-logits" \
   --mode="normal" \
+  --merge="freq" \
   --num_average_groups=45 \
   --n_sentences=32 \
   --train_batch_size=4 \
   --eval_batch_size=32 \
   --partition=1 \
-  --output_path="/app/results/mc-smoe/qwen/merge-45e/freq-dom-router-logits-group-zipit-merge" |& tee results/log_qwen_45e_freq-dom-router-logits-group-zipit-merge
+  --output_path="/app/results/mc-smoe/qwen/merge-45e/freq-dom-router-logits-group-freq-merge-32" |& tee results/log_qwen_45e_freq-dom-router-logits-group-freq-merge-32
 
 accelerate launch --config_file static/finetune_config.yaml \
   --main_process_port 29512 mcsmoe/msmoe-merging-qwen.py \
@@ -26,27 +27,14 @@ accelerate launch --config_file static/finetune_config.yaml \
   --task="winogrande,arc_challenge,arc_easy,boolq,hellaswag,mmlu,openbookqa,rte" \
   --dominant="frequency" \
   --similarity_base="router-logits" \
-  --mode="activation-with-router-logits" \
+  --mode="normal" \
+  --merge="freq" \
   --num_average_groups=45 \
-  --n_sentences=32 \
+  --n_sentences=128 \
   --train_batch_size=4 \
   --eval_batch_size=32 \
   --partition=1 \
-  --output_path="/app/results/mc-smoe/qwen/merge-45e/freq-dom-router-logits-group-zipit-activation-with-router-logits-merge" |& tee results/log_qwen_45e_freq-dom-router-logits-group-zipit-activation-with-router-logits-merge
-
-accelerate launch --config_file static/finetune_config.yaml \
-  --main_process_port 29512 mcsmoe/msmoe-merging-qwen.py \
-  --model_name="Qwen/Qwen1.5-MoE-A2.7B-Chat" \
-  --task="winogrande,arc_challenge,arc_easy,boolq,hellaswag,mmlu,openbookqa,rte" \
-  --dominant="frequency" \
-  --similarity_base="router-logits" \
-  --mode="input-weight" \
-  --num_average_groups=45 \
-  --n_sentences=32 \
-  --train_batch_size=4 \
-  --eval_batch_size=32 \
-  --partition=1 \
-  --output_path="/app/results/mc-smoe/qwen/merge-45e/freq-dom-router-logits-group-zipit-input-weight-merge" |& tee results/log_qwen_45e_freq-dom-router-logits-group-zipit-input-weight-merge
+  --output_path="/app/results/mc-smoe/qwen/merge-45e/freq-dom-router-logits-group-freq-merge-128" |& tee results/log_qwen_45e_freq-dom-router-logits-group-freq-merge-128
 
 accelerate launch --config_file static/finetune_config.yaml \
   --main_process_port 29512 mcsmoe/msmoe-merging-qwen.py \
@@ -55,12 +43,13 @@ accelerate launch --config_file static/finetune_config.yaml \
   --dominant="frequency" \
   --similarity_base="weight" \
   --mode="normal" \
+  --merge="freq" \
   --num_average_groups=45 \
   --n_sentences=32 \
   --train_batch_size=4 \
   --eval_batch_size=32 \
   --partition=1 \
-  --output_path="/app/results/mc-smoe/qwen/merge-45e/freq-dom-weight-group-zipit-merge" |& tee results/log_qwen_45e_freq-dom-weight-group-zipit-merge
+  --output_path="/app/results/mc-smoe/qwen/merge-45e/freq-dom-weight-group-freq-merge-32" |& tee results/log_qwen_45e_freq-dom-weight-group-freq-merge-32
 
 accelerate launch --config_file static/finetune_config.yaml \
   --main_process_port 29512 mcsmoe/msmoe-merging-qwen.py \
@@ -68,27 +57,14 @@ accelerate launch --config_file static/finetune_config.yaml \
   --task="winogrande,arc_challenge,arc_easy,boolq,hellaswag,mmlu,openbookqa,rte" \
   --dominant="frequency" \
   --similarity_base="weight" \
-  --mode="activation-with-router-logits" \
+  --mode="normal" \
+  --merge="freq" \
   --num_average_groups=45 \
-  --n_sentences=32 \
+  --n_sentences=128 \
   --train_batch_size=4 \
   --eval_batch_size=32 \
   --partition=1 \
-  --output_path="/app/results/mc-smoe/qwen/merge-45e/freq-dom-weight-group-zipit-activation-with-router-logits-merge" |& tee results/log_qwen_45e_freq-dom-weight-group-zipit-activation-with-router-logits-merge
-
-accelerate launch --config_file static/finetune_config.yaml \
-  --main_process_port 29512 mcsmoe/msmoe-merging-qwen.py \
-  --model_name="Qwen/Qwen1.5-MoE-A2.7B-Chat" \
-  --task="winogrande,arc_challenge,arc_easy,boolq,hellaswag,mmlu,openbookqa,rte" \
-  --dominant="frequency" \
-  --similarity_base="weight" \
-  --mode="input-weight" \
-  --num_average_groups=45 \
-  --n_sentences=32 \
-  --train_batch_size=4 \
-  --eval_batch_size=32 \
-  --partition=1 \
-  --output_path="/app/results/mc-smoe/qwen/merge-45e/freq-dom-weight-group-zipit-input-weight-merge" |& tee results/log_qwen_45e_freq-dom-weight-group-zipit-input-weight-merge
+  --output_path="/app/results/mc-smoe/qwen/merge-45e/freq-dom-weight-group-freq-merge-128" |& tee results/log_qwen_45e_freq-dom-weight-group-freq-merge-128
 
 accelerate launch --config_file static/finetune_config.yaml \
   --main_process_port 29512 mcsmoe/msmoe-merging-qwen.py \
@@ -97,12 +73,13 @@ accelerate launch --config_file static/finetune_config.yaml \
   --dominant="frequency" \
   --similarity_base="expert-output" \
   --mode="normal" \
+  --merge="freq" \
   --num_average_groups=45 \
   --n_sentences=32 \
   --train_batch_size=4 \
   --eval_batch_size=32 \
   --partition=1 \
-  --output_path="/app/results/mc-smoe/qwen/merge-45e/freq-dom-expert-output-group-zipit-merge" |& tee results/log_qwen_45e_freq-dom-expert-output-group-zipit-merge
+  --output_path="/app/results/mc-smoe/qwen/merge-45e/freq-dom-expert-output-group-freq-merge-32" |& tee results/log_qwen_45e_freq-dom-expert-output-group-freq-merge-32
 
 accelerate launch --config_file static/finetune_config.yaml \
   --main_process_port 29512 mcsmoe/msmoe-merging-qwen.py \
@@ -110,13 +87,74 @@ accelerate launch --config_file static/finetune_config.yaml \
   --task="winogrande,arc_challenge,arc_easy,boolq,hellaswag,mmlu,openbookqa,rte" \
   --dominant="frequency" \
   --similarity_base="expert-output" \
-  --mode="activation-with-router-logits" \
+  --mode="normal" \
+  --merge="freq" \
   --num_average_groups=45 \
+  --n_sentences=128 \
+  --train_batch_size=4 \
+  --eval_batch_size=32 \
+  --partition=1 \
+  --output_path="/app/results/mc-smoe/qwen/merge-45e/freq-dom-expert-output-group-freq-merge-128" |& tee results/log_qwen_45e_freq-dom-expert-output-group-freq-merge-128
+
+accelerate launch --config_file static/finetune_config.yaml \
+  --main_process_port 29512 mcsmoe/msmoe-merging-qwen.py \
+  --model_name="Qwen/Qwen1.5-MoE-A2.7B-Chat" \
+  --task="winogrande,arc_challenge,arc_easy,boolq,hellaswag,mmlu,openbookqa,rte" \
+  --dominant="frequency" \
+  --similarity_base="router-logits" \
+  --mode="normal" \
+  --merge="freq" \
+  --num_average_groups=30 \
   --n_sentences=32 \
   --train_batch_size=4 \
   --eval_batch_size=32 \
   --partition=1 \
-  --output_path="/app/results/mc-smoe/qwen/merge-45e/freq-dom-expert-output-group-zipit-activation-with-router-logits-merge" |& tee results/log_qwen_45e_freq-dom-expert-output-group-zipit-activation-with-router-logits-merge
+  --output_path="/app/results/mc-smoe/qwen/merge-30e/freq-dom-router-logits-group-freq-merge-32" |& tee results/log_qwen_30e_freq-dom-router-logits-group-freq-merge-32
+
+accelerate launch --config_file static/finetune_config.yaml \
+  --main_process_port 29512 mcsmoe/msmoe-merging-qwen.py \
+  --model_name="Qwen/Qwen1.5-MoE-A2.7B-Chat" \
+  --task="winogrande,arc_challenge,arc_easy,boolq,hellaswag,mmlu,openbookqa,rte" \
+  --dominant="frequency" \
+  --similarity_base="router-logits" \
+  --mode="normal" \
+  --merge="freq" \
+  --num_average_groups=30 \
+  --n_sentences=128 \
+  --train_batch_size=4 \
+  --eval_batch_size=32 \
+  --partition=1 \
+  --output_path="/app/results/mc-smoe/qwen/merge-30e/freq-dom-router-logits-group-freq-merge-128" |& tee results/log_qwen_30e_freq-dom-router-logits-group-freq-merge-128
+
+accelerate launch --config_file static/finetune_config.yaml \
+  --main_process_port 29512 mcsmoe/msmoe-merging-qwen.py \
+  --model_name="Qwen/Qwen1.5-MoE-A2.7B-Chat" \
+  --task="winogrande,arc_challenge,arc_easy,boolq,hellaswag,mmlu,openbookqa,rte" \
+  --dominant="frequency" \
+  --similarity_base="weight" \
+  --mode="normal" \
+  --merge="freq" \
+  --num_average_groups=30 \
+  --n_sentences=32 \
+  --train_batch_size=4 \
+  --eval_batch_size=32 \
+  --partition=1 \
+  --output_path="/app/results/mc-smoe/qwen/merge-30e/freq-dom-weight-group-freq-merge-32" |& tee results/log_qwen_30e_freq-dom-weight-group-freq-merge-32
+
+accelerate launch --config_file static/finetune_config.yaml \
+  --main_process_port 29512 mcsmoe/msmoe-merging-qwen.py \
+  --model_name="Qwen/Qwen1.5-MoE-A2.7B-Chat" \
+  --task="winogrande,arc_challenge,arc_easy,boolq,hellaswag,mmlu,openbookqa,rte" \
+  --dominant="frequency" \
+  --similarity_base="weight" \
+  --mode="normal" \
+  --merge="freq" \
+  --num_average_groups=30 \
+  --n_sentences=128 \
+  --train_batch_size=4 \
+  --eval_batch_size=32 \
+  --partition=1 \
+  --output_path="/app/results/mc-smoe/qwen/merge-30e/freq-dom-weight-group-freq-merge-128" |& tee results/log_qwen_30e_freq-dom-weight-group-freq-merge-128
 
 accelerate launch --config_file static/finetune_config.yaml \
   --main_process_port 29512 mcsmoe/msmoe-merging-qwen.py \
@@ -124,10 +162,26 @@ accelerate launch --config_file static/finetune_config.yaml \
   --task="winogrande,arc_challenge,arc_easy,boolq,hellaswag,mmlu,openbookqa,rte" \
   --dominant="frequency" \
   --similarity_base="expert-output" \
-  --mode="input-weight" \
-  --num_average_groups=45 \
+  --mode="normal" \
+  --merge="freq" \
+  --num_average_groups=30 \
   --n_sentences=32 \
   --train_batch_size=4 \
   --eval_batch_size=32 \
   --partition=1 \
-  --output_path="/app/results/mc-smoe/qwen/merge-45e/freq-dom-expert-output-group-zipit-input-weight-merge" |& tee results/log_qwen_45e_freq-dom-expert-output-group-zipit-input-weight-merge
+  --output_path="/app/results/mc-smoe/qwen/merge-30e/freq-dom-expert-output-group-freq-merge-32" |& tee results/log_qwen_30e_freq-dom-expert-output-group-freq-merge-32
+
+accelerate launch --config_file static/finetune_config.yaml \
+  --main_process_port 29512 mcsmoe/msmoe-merging-qwen.py \
+  --model_name="Qwen/Qwen1.5-MoE-A2.7B-Chat" \
+  --task="winogrande,arc_challenge,arc_easy,boolq,hellaswag,mmlu,openbookqa,rte" \
+  --dominant="frequency" \
+  --similarity_base="expert-output" \
+  --mode="normal" \
+  --merge="freq" \
+  --num_average_groups=30 \
+  --n_sentences=128 \
+  --train_batch_size=4 \
+  --eval_batch_size=32 \
+  --partition=1 \
+  --output_path="/app/results/mc-smoe/qwen/merge-30e/freq-dom-expert-output-group-freq-merge-128" |& tee results/log_qwen_30e_freq-dom-expert-output-group-freq-merge-128
