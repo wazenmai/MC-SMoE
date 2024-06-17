fix: kl-dom Qwen and add script

mcsmoe/merging/grouping_qwen.py
@@ -1,4 +1,6 @@
 import os
+import pickle
+import time
 from copy import deepcopy
 from typing import Dict, List, Optional, Tuple
 from types import MethodType
@@ -41,7 +43,7 @@ class ExpertsGrouperForQwen2MoE(object):
 
         self.num_experts = config.num_experts
         self.d_model = config.hidden_size
-        self.d_ff = config.intermediate_size
+        self.d_ff = config.moe_intermediate_size
         self.top_k = config.num_experts_per_tok
 
         self.sparse_layer_indices = list(range(0, config.num_hidden_layers))
@@ -238,6 +240,11 @@ class ExpertsGrouperForQwen2MoE(object):
             model: Qwen2MoeForCausalLM,
             dataloader: DataLoader = None
     ):
+        # if os.path.exists("similarity.pkl"):
+        #     with open("similarity.pkl", "rb") as f:
+        #         self._similarity_state_dict = pickle.load(f)
+        #     return
+        
         similarity_list = ["weight", "router-weight", "router-logits", "expert-output"]
         if self.similarity_base not in similarity_list and dataloader is None:
             raise ValueError(
@@ -253,6 +260,9 @@ class ExpertsGrouperForQwen2MoE(object):
             self._compute_all_similarities_by_expert_outputs(model, dataloader)
         else:
             raise NotImplementedError
+        
+        # with open("similarity.pkl", "wb") as f:
+        #     pickle.dump(self._similarity_state_dict, f)
 
     def _compute_all_similarities_by_weight(self, state_dict: Dict[str, torch.Tensor]):
         for layer_idx in tqdm(self.sparse_layer_indices, desc="[MC-SMoE]  Computing similarities by weight..."):
@@ -388,17 +398,20 @@ class ExpertsGrouperForQwen2MoE(object):
             return hook
         
         for layer_idx in self.sparse_layer_indices:
+            _st = time.time()
             _device = model.model.layers[layer_idx].mlp.experts[0].gate_proj.weight.device
             moe_name = f"model.layers.{layer_idx}.mlp"
             print(f"[Process-Start] === Layer {layer_idx} / {len(self.sparse_layer_indices)} -> {moe_name} ===")
 
             # STEP: 1. Compute similarity
-            print(self._similarity_state_dict[moe_name])
+            print(self._similarity_state_dict[moe_name][0])
             
             # STEP: 2. Compute knowledge + Collect activation for zipit merging
             # 2.1 Initialization
             model.eval()
-            model.zero_grad()
+            # model.zero_grad()
+            for p in model.parameters():
+                p.requires_grad_(False)
             moe_pred_kl = torch.zeros(self.num_experts, self.d_ff).to(_device)
             moe_rep_kl = torch.zeros(self.num_experts, self.d_ff).to(_device)
             moe_masks = torch.ones(self.num_experts, self.d_ff, dtype=torch.float16).to(_device)
@@ -419,7 +432,7 @@ class ExpertsGrouperForQwen2MoE(object):
                 )
                 _inputs[e] = []
                 handles.append(
-                    hijack(experts[e].w2, _inputs[e], _hijack_input=True)
+                    hijack(experts[e].down_proj, _inputs[e], _hijack_input=True)
                 )
             handles.append(moe.register_forward_hook(
                 _get_activation_hook(moe_name)
@@ -430,11 +443,11 @@ class ExpertsGrouperForQwen2MoE(object):
             num_tokens = 0
             _index = 0
             print("[Computing] Do forward and measure knowledge on batch")
-            for b, batch inenumerate(dataloader):
+            for b, batch in enumerate(dataloader):
                 print(b, end='')
                 batch = {k: v.to(_device) for k, v in batch.items()}
                 att_mask = batch["attention_mask"].bool().reshape(-1)
-                batch_samples = att_mask.shape[1]
+                batch_samples = batch["attention_mask"].shape[0]
                 num_samples += batch_samples
                 num_tokens += att_mask.sum().item()
                 outputs = model(**batch, output_router_logits=True)
@@ -450,7 +463,7 @@ class ExpertsGrouperForQwen2MoE(object):
                     input=F.log_softmax(outputs.logits / T, dim=1),
                     target=pred,
                     reduction="batchmean"
-                ) * (T ** 2) / 100
+                ) * (T ** 2)
                 if kl_div > 10:
                     kl_div /= 1000
                 print(f"kl_div: {kl_div.item()}")
@@ -469,9 +482,12 @@ class ExpertsGrouperForQwen2MoE(object):
                 del routing_weights, selected_experts
                 for e in range(self.num_experts):
                     # get feature
-                    token_id = (expert_index == e).nonzero()
-                    number_of_tokens = token_id.shape[0]
-                    _features = _inputs[e][-1][:number_of_tokens].to(torch.float32).to(_device)
+                    # token_id = (expert_index == e).nonzero()
+                    # number_of_tokens = token_id.shape[0]
+                    # print(f"_inputs: {len(_inputs[e])}, {_inputs[e][-1].shape}, number_of_tokens: {number_of_tokens}")
+                    # _features = _inputs[e][-1][:number_of_tokens].to(torch.float32).to(_device)
+                    # print(f"_inputs: {len(_inputs[e])}, {_inputs[e][-1].shape}")
+                    _features = _inputs[e][-1].to(torch.float32).to(_device)
 
                     # get weigth and calculate representational knowledge
                     _weight = experts[e].down_proj.weight.detach()
@@ -483,7 +499,7 @@ class ExpertsGrouperForQwen2MoE(object):
 
                     del _inputs[e][-1], _features, _weight, grad
                 moe_masks.grad.zero_()
-            
+            print(torch.cuda.memory_summary())
             # 2.4 Average score
             moe_pred_kl /= num_samples
             moe_rep_kl /= num_tokens
@@ -504,7 +520,7 @@ class ExpertsGrouperForQwen2MoE(object):
             del handles, moe_pred_kl, moe_rep_kl, moe_masks, _inputs
 
             # STEP: 3. Choose dominant experts by knowledge, group them by similarity
-            print(f"[Process-Start] === Choose dominant experts by knowledge, group them by similarity {similarity_base} ===")
+            print(f"[Process-Grouping] === Choose dominant experts by knowledge, group them by similarity {self.similarity_base} ===")
             indices_sorted_by_kl = torch.argsort(moe_scores, descending=True).cpu()
             core_expert_indices = indices_sorted_by_kl[:num_groups]
             core_experts[moe_name] = core_expert_indices.tolist()
@@ -530,17 +546,17 @@ class ExpertsGrouperForQwen2MoE(object):
                         router_weight = []
                         for j in range(len(forwarded_hidden_states[i])): # one token
                             for r, ind in enumerate(router_indices[i][j]): # token's router-logits and expert-index
-                                if expert_idx == ind:
+                                if e == ind:
                                     batch_tensor[j] = True
                                     router_weight.append(router_weights[i][j][r])
                         router_weight = torch.tensor(router_weight).unsqueeze(1).to(forwarded_hidden_states[i].dtype) # .cpu()
                         hidden_states_list.append(forwarded_hidden_states[i][batch_tensor] * router_weight)
                     else:
                         for j in range(len(forwarded_hidden_states[i])): # one token
-                            if expert_idx in router_indices[i][j]:
+                            if e in router_indices[i][j]:
                                 batch_tensor[j] = True
                         hidden_states_list.append(forwarded_hidden_states[i][batch_tensor])
-                layer_forwarded_hidden_states += (torch.cat(hidden_states_list, dim=0))
+                layer_forwarded_hidden_states += (torch.cat(hidden_states_list, dim=0),)
             model.model.layers[layer_idx].mlp = _merge_moe_experts_within_and_across_models(
                 moe=moe,
                 group_labels=group_labels,
@@ -553,6 +569,7 @@ class ExpertsGrouperForQwen2MoE(object):
 
             del layer_forwarded_hidden_states
             forwarded_hidden_states = []
+            print(f"[Process-End] === Layer {layer_idx} / {len(self.sparse_layer_indices)}, cost {time.time() - _st:2f}s ===")
         self.core_experts = core_experts
         return model
 
@@ -644,6 +661,7 @@ def merge_qwen_moe_by_activation_matching_within_and_across_models(
     if mini_batch_size is None:
         mini_batch_size = forwarded_hidden_states.shape[0]
 
+    _device = forwarded_hidden_states.device
     ffn_all_gate_proj = torch.cat([ffn.gate_proj.weight.data for ffn in ffn_list], dim=0)
     ffn_all_down_proj = torch.cat([ffn.down_proj.weight.data for ffn in ffn_list], dim=1)
     ffn_all_up_proj = torch.cat([ffn.up_proj.weight.data for ffn in ffn_list], dim=0)
@@ -653,7 +671,10 @@ def merge_qwen_moe_by_activation_matching_within_and_across_models(
     concat_ffn.gate_proj.weight.data = ffn_all_gate_proj
     concat_ffn.down_proj.weight.data = ffn_all_down_proj
     concat_ffn.up_proj.weight.data = ffn_all_up_proj
-    concat_ffn = concat_ffn.eval().to(forwarded_hidden_states.device)
+    # concat_ffn = concat_ffn.eval()
+    concat_ffn.gate_proj.weight.to(_device)
+    concat_ffn.up_proj.weight.to(_device)
+    concat_ffn.down_proj.weight.to(_device)
     
     activations = []
     
@@ -802,7 +823,7 @@ def _merge_moe_experts_within_and_across_models(
         usage_frequencies: Optional[torch.Tensor] = None,
 ) -> Qwen2MoeSparseMoeBlock:
 
-    moe.expert_dict = {}
+    # moe.expert_dict = {}
     input_weight = None
 
     for label in group_labels.unique():
@@ -835,15 +856,15 @@ def _merge_moe_experts_within_and_across_models(
         moe.experts[expert_indices[0]].down_proj.weight.copy_(merged_expert.down_proj.weight)
         moe.experts[expert_indices[0]].up_proj.weight.copy_(merged_expert.up_proj.weight)
 
-        moe.expert_dict[expert_indices[0].item()] = expert_indices[0].item()
+        # moe.expert_dict[expert_indices[0].item()] = expert_indices[0].item()
 
         for expert_idx in expert_indices[1:]:
             # Binding merged experts to the first of them
-            # moe.experts[expert_idx] = moe.experts[expert_indices[0]]
-            moe.expert_dict[expert_idx.item()] = expert_indices[0].item()
-            moe.experts[expert_idx.item()] = None
-    print(moe.expert_dict)
-    moe.forward = MethodType(merged_qwen2moe_forward, moe)
+            moe.experts[expert_idx] = moe.experts[expert_indices[0]]
+            # moe.expert_dict[expert_idx.item()] = expert_indices[0].item()
+            # moe.experts[expert_idx.item()] = None
+    # print(moe.expert_dict)
+    # moe.forward = MethodType(merged_qwen2moe_forward, moe)
     return moe
 
 @torch.no_grad()