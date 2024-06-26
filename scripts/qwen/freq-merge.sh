eb79d8ab071ca5ca01535ae328d53752cb7cb083
wazenmai <iotbwu0101@gmail.com>
2024-06-26 17:59:05 +0800

    update qwen script

--- scripts/qwen/freq-merge.sh
+++ scripts/qwen/freq-merge.sh
@@ -108,3 +108,76 @@ accelerate launch --config_file static/finetune_config.yaml \
   --merge="no" \
   --num_average_groups=30 \
   --output_path="/app/results/mc-smoe/qwen/merge-30e/freq-dom-expert-output-group-freq-merge-128" |& tee results/log_new_qwen_30e_freq-dom-expert-output-group-freq-merge-128
+
+# zipit-merge
+accelerate launch --config_file static/finetune_config.yaml \
+  --main_process_port 29512 mcsmoe/msmoe-merging-qwen.py \
+  --model_name="/app/results/mc-smoe/qwen/merge-45e/freq-dom-expert-output-group-zipit-activation-with-router-logits-merge/model.pth" \
+  --task="winogrande,arc_challenge,arc_easy,boolq,hellaswag,mmlu,openbookqa,rte" \
+  --merge="no" \
+  --num_average_groups=30 \
+  --output_path="/app/results/mc-smoe/qwen/merge-45e/freq-dom-expert-output-group-zipit-activation-with-router-logits-merge" |& tee results/log_new_qwen_45e_freq-dom-expert-output-group-zipit-activation-with-router-logits-merge
+
+accelerate launch --config_file static/finetune_config.yaml \
+  --main_process_port 29512 mcsmoe/msmoe-merging-qwen.py \
+  --model_name="/app/results/mc-smoe/qwen/merge-45e/freq-dom-expert-output-group-zipit-input-weight-merge/model.pth" \
+  --task="winogrande,arc_challenge,arc_easy,boolq,hellaswag,mmlu,openbookqa,rte" \
+  --merge="no" \
+  --num_average_groups=30 \
+  --output_path="/app/results/mc-smoe/qwen/merge-45e/freq-dom-expert-output-group-zipit-input-weight-merge" |& tee results/log_new_qwen_45e_freq-dom-expert-output-group-zipit-input-weight-merge
+
+accelerate launch --config_file static/finetune_config.yaml \
+  --main_process_port 29512 mcsmoe/msmoe-merging-qwen.py \
+  --model_name="/app/results/mc-smoe/qwen/merge-45e/freq-dom-expert-output-group-zipit-merge/model.pth" \
+  --task="winogrande,arc_challenge,arc_easy,boolq,hellaswag,mmlu,openbookqa,rte" \
+  --merge="no" \
+  --num_average_groups=30 \
+  --output_path="/app/results/mc-smoe/qwen/merge-45e/freq-dom-expert-output-group-zipit-merge" |& tee results/log_new_qwen_45e_freq-dom-expert-output-group-zipit-merge
+
+accelerate launch --config_file static/finetune_config.yaml \
+  --main_process_port 29512 mcsmoe/msmoe-merging-qwen.py \
+  --model_name="/app/results/mc-smoe/qwen/merge-45e/freq-dom-router-logits-group-zipit-activation-with-router-logits-merge/model.pth" \
+  --task="winogrande,arc_challenge,arc_easy,boolq,hellaswag,mmlu,openbookqa,rte" \
+  --merge="no" \
+  --num_average_groups=30 \
+  --output_path="/app/results/mc-smoe/qwen/merge-45e/freq-dom-router-logits-group-zipit-activation-with-router-logits-merge" |& tee results/log_new_qwen_45e_freq-dom-router-logits-group-zipit-activation-with-router-logits-merge
+
+accelerate launch --config_file static/finetune_config.yaml \
+  --main_process_port 29512 mcsmoe/msmoe-merging-qwen.py \
+  --model_name="/app/results/mc-smoe/qwen/merge-45e/freq-dom-router-logits-group-zipit-input-weight-merge/model.pth" \
+  --task="winogrande,arc_challenge,arc_easy,boolq,hellaswag,mmlu,openbookqa,rte" \
+  --merge="no" \
+  --num_average_groups=30 \
+  --output_path="/app/results/mc-smoe/qwen/merge-45e/freq-dom-router-logits-group-zipit-input-weight-merge" |& tee results/log_new_qwen_45e_freq-dom-router-logits-group-zipit-input-weight-merge
+
+accelerate launch --config_file static/finetune_config.yaml \
+  --main_process_port 29512 mcsmoe/msmoe-merging-qwen.py \
+  --model_name="/app/results/mc-smoe/qwen/merge-45e/freq-dom-router-logits-group-zipit-merge/model.pth" \
+  --task="winogrande,arc_challenge,arc_easy,boolq,hellaswag,mmlu,openbookqa,rte" \
+  --merge="no" \
+  --num_average_groups=30 \
+  --output_path="/app/results/mc-smoe/qwen/merge-45e/freq-dom-router-logits-group-zipit-merge" |& tee results/log_new_qwen_45e_freq-dom-router-logits-group-zipit-merge
+
+accelerate launch --config_file static/finetune_config.yaml \
+  --main_process_port 29512 mcsmoe/msmoe-merging-qwen.py \
+  --model_name="/app/results/mc-smoe/qwen/merge-45e/freq-dom-weight-group-zipit-activation-with-router-logits-merge/model.pth" \
+  --task="winogrande,arc_challenge,arc_easy,boolq,hellaswag,mmlu,openbookqa,rte" \
+  --merge="no" \
+  --num_average_groups=30 \
+  --output_path="/app/results/mc-smoe/qwen/merge-45e/freq-dom-weight-group-zipit-activation-with-router-logits-merge" |& tee results/log_new_qwen_45e_freq-dom-weight-group-zipit-activation-with-router-logits-merge
+
+accelerate launch --config_file static/finetune_config.yaml \
+  --main_process_port 29512 mcsmoe/msmoe-merging-qwen.py \
+  --model_name="/app/results/mc-smoe/qwen/merge-45e/freq-dom-weight-group-zipit-input-weight-merge/model.pth" \
+  --task="winogrande,arc_challenge,arc_easy,boolq,hellaswag,mmlu,openbookqa,rte" \
+  --merge="no" \
+  --num_average_groups=30 \
+  --output_path="/app/results/mc-smoe/qwen/merge-45e/freq-dom-weight-group-zipit-input-weight-merge" |& tee results/log_new_qwen_45e_freq-dom-weight-group-zipit-input-weight-merge
+
+accelerate launch --config_file static/finetune_config.yaml \
+  --main_process_port 29512 mcsmoe/msmoe-merging-qwen.py \
+  --model_name="/app/results/mc-smoe/qwen/merge-45e/freq-dom-weight-group-zipit-merge/model.pth" \
+  --task="winogrande,arc_challenge,arc_easy,boolq,hellaswag,mmlu,openbookqa,rte" \
+  --merge="no" \
+  --num_average_groups=30 \
+  --output_path="/app/results/mc-smoe/qwen/merge-45e/freq-dom-weight-group-zipit-merge" |& tee results/log_new_qwen_45e_freq-dom-weight-group-zipit-merge
